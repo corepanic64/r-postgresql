4edd48ef14110d0670e3b7dd99fb7f0b3efdcf65
Toxir Isakulov <isakulovtohir902@gmail.com>
2026-07-31 11:00:25 +0500

    fucking did it, finally writing to postgres

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+
+mod model;
+use model::{create_user, get_user};
+
+#[tokio::main]
+async fn main() {
+    let db_env = dotenv().ok();
+
+    let datebase_url = env::var("DATABASE_URL").expect("DATEBASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&datebase_url)
+        .await
+        .expect("Failed to create pool");
+
+    create_user(&pool, "akmal", "akmal@gmail.com")
+        .await
+        .unwrap();
+    let user = get_user(&pool, 1).await.unwrap();
+    println!("User : {:#?}", user);
+    println!("User operations completed.");
 }
---
+++ src/model.rs
@@ -0,0 +1,23 @@
+#[derive(sqlx::FromRow, Debug)]
+pub struct User {
+    pub id: i32,
+    pub name: String,
+    pub email: String,
+}
+
+pub async fn create_user(pool: &sqlx::PgPool, name: &str, email: &str) -> Result<(), sqlx::Error> {
+    sqlx::query("INSERT INTO users (name, email) VALUES ($1, $2)")
+        .bind(name)
+        .bind(email)
+        .execute(pool)
+        .await?;
+    Ok(())
+}
+
+pub async fn get_user(pool: &sqlx::PgPool, id: i32) -> Result<User, sqlx::Error> {
+    let user = sqlx::query_as::<_, User>("SELECT * FROM users WHERE id = $1")
+        .bind(id)
+        .fetch_one(pool)
+        .await?;
+    Ok(user)
+}
