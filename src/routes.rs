6d5e8420485c06f3d69bff68dad686af269fb3d7
Toxir Isakulov <isakulovtohir902@gmail.com>
2026-08-12 11:02:21 +0500

    nice layers

--- src/routes.rs
+++ src/routes.rs
@@ -1,10 +1,14 @@
 use axum::{
     Router,
+    extract::Request,
+    middleware::{self, Next},
+    response::Response,
     routing::{get, post},
 };
 use dotenv::dotenv;
 use sqlx::{Pool, Postgres};
 use std::env;
+use tower::{Layer, ServiceBuilder};
 
 use crate::auth::{login::api::login, register::api::register};
 use crate::users::api::{create_user, delete_user, get_user_by_id, get_users, update_user};
@@ -13,16 +17,33 @@ pub async fn init_routes(pool: Pool<Postgres>) {
     dotenv().ok();
     let url = env::var("URL").expect("URL must be stet");
 
-    let app = Router::new()
-        .route("/register", post(register))
-        .route("/login", post(login))
+    let users_routes = Router::new()
         .route("/users", get(get_users).post(create_user))
-        .route(
-            "/users/{id}",
-            get(get_user_by_id).put(update_user).delete(delete_user),
-        )
+        .route_layer(middleware::from_fn(my_user_layer));
+
+    let alohida = Router::new().route(
+        "/users/{id}",
+        get(get_user_by_id).put(update_user).delete(delete_user),
+    );
+    let auth_routes = Router::new()
+        .route("/register", post(register))
+        .route("/login", post(login));
+    let app = Router::new()
+        .merge(users_routes)
+        .merge(auth_routes)
+        .merge(alohida)
+        .layer(middleware::from_fn(my_layer))
         .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind(&url).await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn my_layer(request: Request, next: Next) -> Response {
+    println!("OBSHIY LAYERDAN OTDIM");
+    next.run(request).await
+}
+async fn my_user_layer(request: Request, next: Next) -> Response {
+    println!("USERS LAYERDAN OTDIM");
+    next.run(request).await
 }
